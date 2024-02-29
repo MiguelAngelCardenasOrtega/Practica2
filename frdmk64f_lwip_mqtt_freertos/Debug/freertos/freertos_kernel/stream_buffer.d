aa90e218025745a93c9386c31607647b1737fd84
mcardenas <mcardenas@a2etechnologies.com>
2024-02-28 18:37:52 -0600

    -First commit with the lwip and mqtt SDK example almost untouched

---
+++ frdmk64f_lwip_mqtt_freertos/Debug/freertos/freertos_kernel/stream_buffer.d
@@ -0,0 +1,23 @@
+freertos/freertos_kernel/stream_buffer.o \
+ freertos/freertos_kernel/stream_buffer.d: \
+ ../freertos/freertos_kernel/stream_buffer.c \
+ C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/FreeRTOS.h \
+ C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\source/FreeRTOSConfig.h \
+ C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/projdefs.h \
+ C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/portable.h \
+ C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/deprecated_definitions.h \
+ C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\portable\GCC\ARM_CM4F/portmacro.h \
+ C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/mpu_wrappers.h \
+ C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/task.h \
+ C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/list.h \
+ C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/stream_buffer.h
+C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/FreeRTOS.h:
+C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\source/FreeRTOSConfig.h:
+C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/projdefs.h:
+C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/portable.h:
+C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/deprecated_definitions.h:
+C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\portable\GCC\ARM_CM4F/portmacro.h:
+C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/mpu_wrappers.h:
+C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/task.h:
+C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/list.h:
+C:\Users\mcardenas\Documents\MCUXpressoIDE_11.6.1_8255\Iteso\frdmk64f_lwip_mqtt_freertos\freertos\freertos_kernel\include/stream_buffer.h:
